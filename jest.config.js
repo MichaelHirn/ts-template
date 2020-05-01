39df7834f2a14f7a957369f0511f36ee56279394
Michael Hirn <michael.j.hirn@gmail.com>
2020-05-01 11:58:09 +0100

    chore(jest): use jest.config.json

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  roots: ['./src', './test'],
-  testEnvironment: 'node',
-  transform: {
-    '^.+\\.tsx?$': 'ts-jest',
-  },
-  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-}
